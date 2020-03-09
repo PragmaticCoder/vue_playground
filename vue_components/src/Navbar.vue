14cddf22a08ff0cc89bbbfad4fb60d40ed713372
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-03-09 17:21:57 -0600

    created initial Navbar Component

---
+++ vue_components/src/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav>
+    <ul>
+      <li><a href=""></a>Home</li>
+      <li><a href=""></a>About</li>
+      <li><a href=""></a>Contact</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+nav {
+  text-align: center;
+}
+nav ul {
+  padding: 0;
+}
+
+nav li {
+  display: inline-block;
+  list-style-type: none;
+  margin: 0;
+}
+</style>
