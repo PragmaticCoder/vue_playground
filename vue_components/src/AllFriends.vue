b830405d238a1cc56291bbe96abbe59ba604ef91
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-03-09 17:50:58 -0600

    displaying all friends

--- vue_components/src/AllFriends.vue
+++ vue_components/src/AllFriends.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="all-friends">
     <h2>All Friends</h2>
+    <div v-for="(friend, index) in friends" :key="index">
+      <span>{{ friend.name }}</span>
+    </div>
   </div>
 </template>
 <script>
