2efa34fc4e392f02b0ffd35bad3a927770d3b643
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-03-10 00:13:53 -0600

    cloned vue components tutorial and created another separate tutorial project for vue hooks

---
+++ vue_hooks/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <Navbar />
+    <AllFriends :friends="friends" @delete="deleteFriend" />
+    <OnlineFriends :friends="friends" />
+  </div>
+</template>
+
+<script>
+import Navbar from "./Navbar";
+import AllFriends from "./AllFriends";
+import OnlineFriends from "./OnlineFriends";
+
+export default {
+  name: "app",
+  components: {
+    Navbar,
+    AllFriends,
+    OnlineFriends
+  },
+  data() {
+    return {
+      title: "Welcome to Vue.js Components Tutorial",
+      friends: [
+        { name: "Alex", online: true },
+        { name: "Bob", online: false },
+        { name: "Doe", online: true },
+        { name: "George", online: false }
+      ]
+    };
+  },
+  methods: {
+    deleteFriend(payload) {
+      this.friends = this.friends.filter(friend => {
+        return friend.name !== payload.name;
+      });
+    }
+  }
+};
+</script>
+
+<style>
+h1 {
+  color: #444;
+  font-weight: normal;
+  text-align: center;
+}
+</style>
