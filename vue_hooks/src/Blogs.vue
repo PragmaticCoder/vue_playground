e27a804d192954882d7b0fea1b189db235476549
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-03-10 00:32:05 -0600

    implemented beforeCreate and created Hooks

---
+++ vue_hooks/src/Blogs.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="blogs">
+    <h2>{{ blogTitle }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Blogs",
+  data() {
+    return {
+      blogTitle: "Blog: Vue Hooks Gotchas"
+    };
+  },
+  beforeCreate() {
+    alert("beforeCreate hook fired!");
+  },
+  created() {
+    alert(
+      "created hook fired! At this point, everything has been created but not rendered!. This is where we can fetch some data from external api for example and display it after"
+    );
+  },
+  afterCreate() {
+    alert("afterCreate hook fired!");
+  }
+};
+</script>
+
+<style></style>
