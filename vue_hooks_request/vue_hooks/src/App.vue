553f647d57fafcfbf5e28c305f3b84e8d2f00681
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-03-10 02:08:18 -0600

    created another new project to demostration axios api call using hooks

---
+++ vue_hooks_request/vue_hooks/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <Navbar />
+    <Blogs />
+  </div>
+</template>
+
+<script>
+import Navbar from "./Navbar";
+import Blogs from "./Blogs";
+
+export default {
+  name: "app",
+  components: {
+    Navbar,
+    Blogs
+  },
+  data() {
+    return {
+      title: "Welcome to Vue.js Hooks Tutorial"
+    };
+  }
+};
+</script>
+
+<style>
+h1 {
+  color: #444;
+  font-weight: normal;
+  text-align: center;
+}
+</style>
