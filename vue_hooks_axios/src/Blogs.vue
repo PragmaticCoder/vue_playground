1017fcbfcc54d575a1c45eb5bcb870573b2c1271
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-03-10 02:52:51 -0600

    created a separate component for Cards

--- vue_hooks_axios/src/Blogs.vue
+++ vue_hooks_axios/src/Blogs.vue
@@ -1,22 +1,19 @@
 <template>
   <div class="blogs">
     <h2>{{ blogTitle }}</h2>
-    <div class="card" v-for="post in posts" :key="post.id">
-      <div class="container">
-        <h4>
-          <b>{{ post.title }}</b>
-        </h4>
-        <p>{{ post.body | snippet }}</p>
-      </div>
-    </div>
+    <Cards :posts="posts" />
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import Cards from "./Cards";
 
 export default {
   name: "Blogs",
+  components: {
+    Cards
+  },
   data() {
     return {
       blogTitle: "Blog: Vue Hooks API Gotchas",
@@ -36,20 +33,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.card {
-  /* Add shadows to create the "card" effect */
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  transition: 0.3s;
-}
-
-/* On mouse-over, add a deeper shadow */
-.card:hover {
-  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-}
-
-/* Add some padding inside the card container */
-.container {
-  padding: 2px 16px;
-}
-</style>
+<style scoped></style>
---
+++ vue_hooks_axios/src/Cards.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="card" v-for="post in posts" :key="post.id">
+      <div class="container">
+        <h4>
+          <b>{{ post.title }}</b>
+        </h4>
+        <p>{{ post.body | snippet }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cards",
+  props: ["posts"],
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  /* Add shadows to create the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+}
+
+/* On mouse-over, add a deeper shadow */
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+
+/* Add some padding inside the card container */
+.container {
+  padding: 2px 16px;
+}
+</style>
