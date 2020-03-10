24f0569deade5819b5c80813e276185dc8da9bf7
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-03-10 00:15:37 -0600

    cleaned up all friends components related stuff

--- vue_hooks/src/App.vue
+++ vue_hooks/src/App.vue
@@ -2,40 +2,21 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <Navbar />
-    <AllFriends :friends="friends" @delete="deleteFriend" />
-    <OnlineFriends :friends="friends" />
   </div>
 </template>
 
 <script>
 import Navbar from "./Navbar";
-import AllFriends from "./AllFriends";
-import OnlineFriends from "./OnlineFriends";
 
 export default {
   name: "app",
   components: {
-    Navbar,
-    AllFriends,
-    OnlineFriends
+    Navbar
   },
   data() {
     return {
-      title: "Welcome to Vue.js Components Tutorial",
-      friends: [
-        { name: "Alex", online: true },
-        { name: "Bob", online: false },
-        { name: "Doe", online: true },
-        { name: "George", online: false }
-      ]
+      title: "Welcome to Vue.js Hooks Tutorial"
     };
-  },
-  methods: {
-    deleteFriend(payload) {
-      this.friends = this.friends.filter(friend => {
-        return friend.name !== payload.name;
-      });
-    }
   }
 };
 </script>
