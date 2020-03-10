de3e4329c1d53f74a7b4b379b36e0e4c173bb886
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-03-10 02:29:52 -0600

    displaying posts

--- vue_hooks_axios/src/Blogs.vue
+++ vue_hooks_axios/src/Blogs.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="blogs">
     <h2>{{ blogTitle }}</h2>
+    <div class="card" v-for="post in posts" :key="post.id">
+      <div class="container">
+        <h4>
+          <b>{{ post.title }}</b>
+        </h4>
+        <p>{{ post.body }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,4 +31,20 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.card {
+  /* Add shadows to create the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+}
+
+/* On mouse-over, add a deeper shadow */
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+
+/* Add some padding inside the card container */
+.container {
+  padding: 2px 16px;
+}
+</style>
